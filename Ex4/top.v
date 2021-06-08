0b8f39cb5e95354acd63b1303e9a5675fee5dee8
venicemcg <69117751+venicemcg@users.noreply.github.com>
2021-06-08 13:56:37 +0100

    Update top.v

--- Ex4/top.v
+++ Ex4/top.v
@@ -23,59 +23,25 @@ module LED(
    input clk, 
    input rst, 
    input button, 
-   output [2:0] colour 
+   output reg [2:0] colour 
    ); 
 
-   // Add the registers and the wires required 
-//CHECK TO SEE IF MORE ARE REQUIRED 
-   reg colour; 
-
-   // User logic 
-   always @(posedge clk) 
-   begin 
-   // Use a series of else if statements to move through the different lighting cases 
-   if (colour==3'b000)
-      colour = 3'b001; 
-   // No check needs to be done on the value of button since if button = 0 or 1 this is always the case 
-   else if (colour==3'b111)
-      colour = 3'b001; 
-   // This reasoning is the same as above 
-   else if (colour==3'b001 && button==1)
-      colour = 3'b010; 
-   // Step through to next colour
-   else if (colour==3'b001 && button==0)
-      colour = colour; 
-   // No change in value of colour occurs 
-   else if (colour==3'b010 && button==1)
-      colour = 3'b011; 
-   // Step through to next colour 
-   else if (colour==3'b010 && button==0)
-      colour = colour; 
-   // No change in value occurs 
-   else if (colour==3'b011 && button==1)
-      colour = 3'b100; 
-   // Step through to next colour 
-   else if (colour==3'b011 && button==0)
-      colour = colour; 
-   // No change in value occurs 
-   else if (colour==3'b100 && button==1)
-      colour = 3'b101; 
-   // Step through to next colour 
-   else if (colour==3'b100 && button==0)
-      colour = colour; 
-   // No change in value occurs 
-   else if (colour==3'b101 && button==1)
-      colour = 3'b110; 
-   // Step through to next colour 
-   else if (colour==3'b101 && button==0)
-      colour = colour; 
-   // No change in value occurs 
-   else if (colour==3'b110 && button==1) 
-      colour = 3'b001; 
-   // Step through to next colour 
-   else if (colour==3'b110 && button==0)
-      colour = colour; 
-   // No change in value occurs 
-   end 
-
+   always @(posedge clk or posedge rst) begin 
+    
+      if (rst == 1)
+         colour <= 'b001;
+      else if (colour == 'b000 || colour == 'b111)
+         colour <= 'b001; 
+      else if (button == 1) begin 
+         case (colour)
+            1 : colour <= 2;
+            2 : colour <= 3; 
+            3 : colour <= 4;
+            4 : colour <= 5; 
+            5 : colour <= 6; 
+            6 : colour <= 1; 
+            default : colour <= 1;
+         endcase:
+      end
+   end
 endmodule
