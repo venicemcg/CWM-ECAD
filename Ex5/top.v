da496c507220c4c9b03db4a9efd036624601e433
venicemcg <69117751+venicemcg@users.noreply.github.com>
2021-06-08 14:49:48 +0100

    Update top.v

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Venice McGauley 
+// Date: 8th June 2021 
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -13,3 +13,44 @@
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/ 100ps 
+
+module ac(input clk, [4:0]temp, 
+          output reg heat, reg cool); 
+  
+// Define any parameters needed
+  parameter turnoncool = 22; 
+  parameter turnonheat = 18; 
+  parameter goaltemp = 20;
+  
+  always @(posedge clk) begin 
+    case ({heat,cool})
+      'b01 : begin
+        // heat = 0 cool = 1 
+        if (temp <= goaltemp) begin 
+          cool <= 0; 
+          heat <= 0; 
+        end 
+      end
+      'b10 : begin 
+        // heat = 1 cool = 0 
+        if (temp >= goaltemp) begin 
+          cool <= 0; 
+          heat <= 0; 
+        end
+      end
+      default : begin 
+        if (temp <= turnonheat) begin 
+          cool <= 0; 
+          heat <= 1; 
+        end else if (temp >= turnoncool) begin 
+          cool <= 1; 
+          heat <= 0; 
+        end else begin 
+          heat <= 0; 
+          cool <= 0; 
+        end
+      end
+    endcase
+  end
+endmodule 
