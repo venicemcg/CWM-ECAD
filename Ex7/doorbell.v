c1937529a122b47366d023033f4c4099bdd91415
venicemcg <69117751+venicemcg@users.noreply.github.com>
2021-06-09 10:40:54 +0100

    Update doorbell.v

--- Ex7/doorbell.v
+++ Ex7/doorbell.v
@@ -1,1 +1,19 @@
+`timescale 1ns / 100ps
 
+module doorbell(
+    //Todo: define inputs and outputs here
+    input a,
+    input b, 
+    input sel, 
+    output out
+    );
+    
+    //Todo: define registers and wires here
+    wire out;
+
+    //Todo: define your logic here   
+
+    // Logic for the output of the multiplexer  
+    assign #5 out = (sel==0)?a:b;             
+      
+endmodule
