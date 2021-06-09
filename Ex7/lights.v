67c085fb0c9b31987bb831889451b3f97b5b60cc
venicemcg <69117751+venicemcg@users.noreply.github.com>
2021-06-09 10:40:01 +0100

    Update lights.v

--- Ex7/lights.v
+++ Ex7/lights.v
@@ -2,18 +2,27 @@
 
 `timescale 1ns / 100ps 
 
-module lights(input rst,clk, button, 
-output reg [2:0]colour); 
-
-// user logic
-initial begin 
-colour = 3'b002; 
-end
-
-always @(posedge clk)
-begin 
-if (rst == 1)
-colour = 3'b000; 
-  else if (button == 0 && (colour == 3'b111 || colour == 3'b000))
-    colour = 3'b001; 
-  else if
+module lights(
+   // Add the inputs and outputs
+   input clk,
+   input rst, 
+   input button, 
+   output reg[2:0]colour
+); 
+   
+   initial begin 
+      colour = 3'b001; 
+   end
+   
+   always @(posedge clk)
+      begin 
+         if ((button == 0 || button == 1 ) && (colour == 3'b111 || colour == 3'b000))
+            colour = 3'b001; 
+         else if (button == 0)
+            colour = colour; 
+         else if ((button == 1) && (colour != 3'b111) && (colour != 3'b110))
+            colour = colour + 1;
+         else
+            colour = 3'b001; 
+      end
+endmodule 
