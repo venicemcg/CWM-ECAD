623b9ce49adf5e1cc82ff393a79ae2b7c84bb86f
venicemcg <69117751+venicemcg@users.noreply.github.com>
2021-06-09 10:47:18 +0100

    Update top.v

--- Ex8/top.v
+++ Ex8/top.v
@@ -12,30 +12,30 @@
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps 
 
-module top(
-    input clk_p,
-    input clk_n,
-     //Todo: add all other ports besides clk_n and clk_p 
-   );
-    
-
-   /* clock infrastructure, do not modify */
-        wire clk_ibufds;
-
-    IBUFDS IBUFDS_sysclk (
-	.I(clk_p),
-	.IB(clk_n),
-	.O(clk_ibufds)
-);
-
-     wire clk; //use this signal as a clock for your design
-        
-     BUFG bufg_clk (
-	.I  (clk_ibufds),
-	.O  (clk)
-      );
-
-//Add logic here
-
-endmodule
+module top(input clk_p, clk_n, rst_n, temperature_0, temperature_1, temperature_2, temperature_3, temperature_4, output heating, cooling); 
+	
+	
+	// clock infrastructure
+	wire clk_ibufds; 
+	
+	IBUFDS IBUFDS_sysclk (
+		.I(clk_p), 
+		.IB(clk_n), 
+		.O(clk_ibufds)
+	); 
+	
+	wire clk; // use this signal as a clock for the design 
+	
+	BUFG bufg_clk(
+		.I (clk_ibufds), 
+		.O (clk)
+	); 
+	
+	// logic
+	wire temp; 
+	assign temp = {temperature_4, temperature_3, temperature_2, temperature_1, temperature_0}; 
+	ac ac(.clk(clk), .temp(temp), .heat(heating), .cool(cooling)); 
+	
+endmodule 
