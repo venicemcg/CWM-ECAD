3ae3c5c1af7adbc37508f2a40bb32d622f443806
venicemcg <69117751+venicemcg@users.noreply.github.com>
2021-06-11 09:47:28 +0100

    Update top_tb.v

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -16,66 +16,63 @@ module top_tb();
   
 // Define any registers and wires 
  reg clk; 
-  reg [4:0] temp; 
-  wire heating;
-  wire cooling; 
-  reg err = 0; 
-  reg up_down = 1; 
-  reg [4:0] state_prev; 
+  reg [2:0]colour; 
+  wire ena; 
+  wire [23:0]rgb;
+  reg err; 
   
+  // Clock generation 
   initial begin 
     clk = 1'b0; 
     forever
-      #(CLK_PERIOD/2) clk=~clk;
+      #(CLK_PERIOD/2) clk=~clk; 
   end
   
+  // trial various 3 bit colours 
+  assign enable = 1; 
   initial begin 
+    err = 0; 
+    colour = 3'b000; 
     forever begin 
-      #CLK_PERIOD
-      if (cooling == 1 && heating == 1 ) begin 
-        $display("Test failed - cooling and heating can not be both on"); 
-        err = 1; 
-      end
+      #10 if (rgb != 24'b000000000000000000000000)
+        begin 
+          err <= 1; 
+        end
+      else
+        begin 
+          err <= err; 
+        end
+    end
+    
+    colour = 3'b111; 
+    forever begin 
+      #10 if (rgb != 24'b111111111111111111111111)
+        begin 
+          err <= 1; 
+        end
+      else
+        begin 
+          err <= err; 
+        end
     end
   end
   
-  // Test by cucling temp between 16 and 24
+  // Finish test and check for success
   initial begin 
-    temp = 5'b10000; 
-    forever begin 
-      if (temp <= 5'b10000) 
-        up_down = 1; 
-      else if (temp >= 5'b11000)
-        up_down = 0; 
-      else if (up_down == 1) begin 
-        #CLK_PERIOD
-        temp = temp + 5'b00001; 
-      end
-      else if (up_down == 0) begin 
-        #CLK_PERIOD
-        temp = temp - 5'b00001; 
-      end
-    end
-    
-    #CLK_PERIOD
-    temp = dir ? temp + 5'b00001 : temp - 5'b00001; 
-    
-    §§§
-  end
-  
-  // Finish test 
-  initial begin 
-    #200 
+    #50
     if (err == 0)
-      $display("test passed"); 
+      $display("TEST PASSED"); 
+    $finish; 
+    if (errr == 1)
+      $display("TEST FAILED"); 
     $finish; 
   end
   
-  ac top(
-    .temp(temp), 
+  // Instantiating converter top
+  converter top(
     .clk(clk), 
-    .heating(heating), 
-    .cooling(cooling)
+    .colour(colour), 
+    .ena(ena), 
+    .rgb(rgb)
   ); 
-  
 endmodule 
