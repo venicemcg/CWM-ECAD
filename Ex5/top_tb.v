1cb8ccda92775695b175e9dfeb963c9afa3a73ae
venicemcg <69117751+venicemcg@users.noreply.github.com>
2021-06-11 09:53:44 +0100

    Update top_tb.v

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Air Conditioning
 // Student Name: Venice McGauley
-// Date: 8th June 2021 
+// Date: 
 //
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
@@ -13,13 +13,16 @@ module top_tb();
   
 // Define any parameters
   parameter CLK_PERIOD = 10; 
+  parameter cooling = 2'b01; 
+  parameter heating = 2'b10; 
+  parameter ideal = 2'b00; 
   
 // Define any registers and wires 
- reg clk; 
-  reg [2:0]colour; 
-  wire ena; 
-  wire [23:0]rgb;
-  reg err; 
+reg clk; 
+  reg [4:0] temp; 
+  wire heating; 
+  wire cooling; 
+  wire [1:0] current_temp = {heating, cooling};
   
   // Clock generation 
   initial begin 
@@ -28,51 +31,46 @@ module top_tb();
       #(CLK_PERIOD/2) clk=~clk; 
   end
   
-  // trial various 3 bit colours 
-  assign enable = 1; 
-  initial begin 
-    err = 0; 
-    colour = 3'b000; 
-    forever begin 
-      #10 if (rgb != 24'b000000000000000000000000)
-        begin 
-          err <= 1; 
-        end
-      else
-        begin 
-          err <= err; 
-        end
-    end
-    
-    colour = 3'b111; 
-    forever begin 
-      #10 if (rgb != 24'b111111111111111111111111)
-        begin 
-          err <= 1; 
-        end
-      else
-        begin 
-          err <= err; 
-        end
-    end
-  end
-  
-  // Finish test and check for success
-  initial begin 
-    #50
-    if (err == 0)
-      $display("TEST PASSED"); 
-    $finish; 
-    if (errr == 1)
-      $display("TEST FAILED"); 
-    $finish; 
-  end
-  
-  // Instantiating converter top
-  converter top(
-    .clk(clk), 
-    .colour(colour), 
-    .ena(ena), 
-    .rgb(rgb)
-  ); 
-endmodule 
+  //start at temp 0
+  temp = 5'b00000; 
+  forever begin 
+    #10 if ((heat == 1 && temp > 20) || (cool == 1  && temp < 20) || (heat == 1 && cool == 1))
+		begin err=1;
+		end
+		else
+		begin err=0;
+		end
+end
+
+//Trial response to high initial temp:
+
+temp = 5'b11111;	
+forever begin
+	#10 if ((heat == 1 && temp > 20) || (cool == 1 && temp < 20) || (heat == 1 && cool == 1)) //confirming no illegal states in subsequenct clock cycles
+		begin err=1;
+		end
+		else
+		begin err=0;
+		end
+
+	end
+end
+
+//repeating but start at mid temp (20)
+
+
+//Todo: Finish test, check for success
+initial begin
+        #50 
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+	if (err==1)
+	  $display("***TEST FAILED! :( ***");
+        $finish;
+      end
+
+//Instantiating ac top:
+ac top(clk,temp,heat,cool);
+
+endmodule
